cf81fcd693e0a1acb10a832dfa4e3051d5d0a27d
Omar Mohamed <o-mohamed@hotmail.com>
2017-07-04 23:35:23 +0200

    Update UVA 10263.cpp

--- UVA/UVA 10263.cpp
+++ UVA/UVA 10263.cpp
@@ -5,14 +5,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int N = 1e3 + 1;
-const double EPS = 1e-6;
-typedef complex<double> point;
-double f(double x0, double y0, double x1, double y1, double x, point c) {
-//		(y0 - y1) * x + (x1 - x0) * y + (x0 * y1 - x1 * y0) = 0;
-//		(y0 - y1) * x + (x0 * y1 - x1 * y0) = -(x1 - x0) * y;
-	double y = ((y0 - y1) * x + (x0 * y1 - x1 * y0)) / -(x1 - x0);
-	return (x - c.real()) * (x - c.real()) + (y - c.imag()) * (y - c.imag());
+
+double f(double x1, double y1, double x2, double y2, double x, double targetX, double targetY) {
+//		(y1 - y2) * x + (x2 - x1) * y + (x1 * y2 - x2 * y1) = 0;
+	double y = ((y1 - y2) * x + (x1 * y2 - x2 * y1)) / -(x2 - x1);
+	return (x - targetX) * (x - targetX) + (y - targetY) * (y - targetY);
 }
 int main() {
 #ifndef ONLINE_JUDGE
@@ -20,7 +17,6 @@ int main() {
 #endif
 	double x, y;
 	while (scanf("%lf %lf", &x, &y) != EOF) {
-		point c(x, y);
 		int n;
 		scanf("%d", &n);
 		double mini = 1e18, resultX = -1e9, resultY = -1e9;
@@ -33,16 +29,16 @@ int main() {
 			for (int j = 0; j < 100; j++) {
 				double l1 = (l * 2 + r) / 3;
 				double l2 = (l + 2 * r) / 3;
-				if (f(x1, y1, x2, y2, l1, c) < f(x1, y1, x2, y2, l2, c)) r = l2;
+				if (f(x1, y1, x2, y2, l1, x, y) < f(x1, y1, x2, y2, l2, x, y)) r = l2;
 				else l = l1;
 			}
-			double dis = f(x1, y1, x2, y2, l, c);
+			double dis = f(x1, y1, x2, y2, l, x, y);
 			if (dis > mini) {
 				continue;
 			}
+			mini = dis;
 			resultX = l;
 			resultY = ((y1 - y2) * resultX + (x1 * y2 - x2 * y1)) / -(x2 - x1);
-			mini = f(x1, y1, x2, y2, l, c);
 			if (x2 == x1) {
 				if (y1 <= y && y <= y2 || y2 <= y && y <= y1)
 					resultY = y;
@@ -56,6 +52,5 @@ int main() {
 		}
 		printf("%.4lf\n%.4lf\n", resultX, resultY);
 	}
-	return 0;
-	
+	return 0;	
 }
