12ece29bbe79771017ffa729d023b9b0fc4cb2e7
Omar Mohamed <o-mohamed@hotmail.com>
2018-02-20 09:52:57 +0200

    Update 1809.cpp

--- POJ/1809.cpp
+++ POJ/1809.cpp
@@ -11,27 +11,6 @@ long long nCr(long long n, long long r) {
 	return nCr(n - 1, r) * n / (n - r);
 }
 int freq[4];
-long long go(int i, int j, vector<int> type) {
-	int curFreq[4]{freq[0] - (type[0] == 0) - (type[1] == 0) - (type[2] == 0),
-				   freq[1] - (type[0] == 1) - (type[1] == 1) - (type[2] == 1),
-				   freq[2] - (type[0] == 2) - (type[1] == 2) - (type[2] == 2),
-				   freq[3] - (type[0] == 3) - (type[1] == 3) - (type[2] == 3)};
-	if (i == 3) {
-		int x1 = type[0] / 2, y1 = type[0] % 2;
-		int x2 = type[1] / 2, y2 = type[1] % 2;
-		int x3 = type[2] / 2, y3 = type[2] % 2;
-		return ((x1 * y2 - y1 * x2 + x2 * y3 - y2 * x3 + x3 * y1 - y3 * x1) % 2 == 0) * nCr(freq[0], curFreq[0]) *
-			   nCr(freq[1], curFreq[1]) * nCr(freq[2], curFreq[2]) * nCr(freq[3], curFreq[3]);
-	}
-	long long ret = 0;
-	for (int k = j; k < 4; ++k) {
-		if (curFreq[k]) {
-			type[i] = k;
-			ret += go(i + 1, k, type);
-		}
-	}
-	return ret;
-}
 int main() {
 	int t, n;
 	scanf("%d", &t);
@@ -44,8 +23,25 @@ int main() {
 			int type = (abs(x) % 2) * 2 + abs(y) % 2;
 			++freq[type];
 		}
-		vector<int> tmp(3);
-		printf("Scenario #%d:\n%lld\n\n", test, go(0, 0, tmp));
+		long long res = 0;
+		for (int type1 = 0; type1 < 4; ++type1) {
+			for (int type2 = type1; type2 < 4; ++type2) {
+				for (int type3 = type2; type3 < 4; ++type3) {
+					int curFreq[4]{freq[0] - (type1 == 0) - (type2 == 0) - (type3 == 0),
+								   freq[1] - (type1 == 1) - (type2 == 1) - (type3 == 1),
+								   freq[2] - (type1 == 2) - (type2 == 2) - (type3 == 2),
+								   freq[3] - (type1 == 3) - (type2 == 3) - (type3 == 3)};
+					int x1 = type1 / 2, y1 = type1 % 2;
+					int x2 = type2 / 2, y2 = type2 % 2;
+					int x3 = type3 / 2, y3 = type3 % 2;
+					res += ((x1 * y2 - y1 * x2 + x2 * y3 - y2 * x3 + x3 * y1 - y3 * x1) % 2 == 0) *
+						   nCr(freq[0], curFreq[0]) * nCr(freq[1], curFreq[1]) * nCr(freq[2], curFreq[2]) *
+						   nCr(freq[3], curFreq[3]);
+					
+				}
+			}
+		}
+		printf("Scenario #%d:\n%lld\n\n", test, res);
 	}
 	return 0;
 }
